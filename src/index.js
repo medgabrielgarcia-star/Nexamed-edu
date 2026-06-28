b9710cd2d8208b64c091c14dcb9b7166e2c0b117
Gabriel <med.gabrielgarcia@gmail.com>
2026-06-28 14:48:54 -0500

    Cambiando todo a mobil-first: rama experiment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const iconToggle = document.querySelector(".toggle");
 const menuToggle = document.querySelector(".header__nav");
 
 iconToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active')
-})
+    menuToggle.classList.toggle('active');
+    iconToggle.classList.toggle('active')
+});
